afb4c7ac0de69cff33c76e5f2eca542e5aa9a54f
Yoshitake Hamano <oed0cow6oy5@gmail.com>
2018-07-22 11:36:56 +0900

    Add sample

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/tarm/serial"
+	"log"
+	"strings"
+	"time"
+)
+
+func main() {
+	c := &serial.Config{Name: "/dev/rfcomm0", Baud: 115200}
+	s, err := serial.OpenPort(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	n, err := s.Write([]byte("info\n"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	buf := make([]byte, 128)
+	for {
+		time.Sleep(time.Millisecond * 500)
+
+		n, err = s.Read(buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+		sbuf := string(buf[:n])
+		log.Printf(sbuf)
+
+		if strings.Contains(sbuf, "result") {
+			break
+		}
+	}
+}
